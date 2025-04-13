08646a94426709c8b61b41c2ccc34ee019b6f407
Chirag Makwana <makwanachirag427@gmail.com>
2025-04-13 17:23:49 +0530

    4- prepared app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
-import dotenv from "dotenv/config.js";
+import path from "path";
+import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import { fileURLToPath } from "url";
+import {dirname} from 'path'
+
+
 
 // routes
 import authRoutes from "./routes/auth.route.js";
@@ -11,16 +16,23 @@ import postRoutes from "./routes/post.route.js";
 
 import connectDB from "./db/connectMongodb.js";
 
+dotenv.config();
+
+// recreate __dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 cloudinary.config({
-    cloud_name : process.env.CLODUINARY_CLOUD_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
-    api_secret : process.env.CLOUDYNARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDYNARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json({limit : "5mb"}));
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
@@ -28,6 +40,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+  
+}
 
 app.listen(PORT, () => {
   connectDB();
